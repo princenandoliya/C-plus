de43ffb34d828e4c8a8135d9f9a2f1cb8d1c07ba
princenandoliya <princenandoliya@gmail.com>
2025-12-24 16:43:17 +0530

    Add files via upload

---
+++ Introduction/baisc1.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+
+int main()
+{
+	int number;
+	cout << "enter your number\n";
+	cin >> number;
+	
+	cout << "your number is \n" << number;
+	
+	
+	
+	return 0;
+}
+
+
